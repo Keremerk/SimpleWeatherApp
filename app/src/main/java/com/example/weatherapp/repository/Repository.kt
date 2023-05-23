da446e35781b4793cc52482d2ab599a1f5d560ff
Kerem Erkubilay <kereme@windowslive.com>
2023-05-23 23:33:52 +0300

    Initial Commit

---
+++ app/src/main/java/com/example/weatherapp/repository/Repository.kt
@@ -0,0 +1,8 @@
+package com.example.weatherapp.repository
+
+import com.example.weatherapp.data.ApiService
+import javax.inject.Inject
+
+class Repository @Inject constructor(private val apiService: ApiService) {
+
+}
---
+++ app/src/main/java/com/example/weatherapp/utils/Constants.kt
@@ -0,0 +1,7 @@
+package com.example.weatherapp.utils
+
+object Constants {
+    const val BASE_URL = "https://api.astria.ai/"
+    const val API_KEY = ""
+
+}
