f4b4bd9706a6e384bc35d8962de3ee7ed42df2b7
Kerem Erkubilay <kereme@windowslive.com>
2023-05-25 01:38:29 +0300

    minor change

--- app/src/main/java/com/example/weatherapp/utils/Constants.kt
+++ app/src/main/java/com/example/weatherapp/utils/Constants.kt
@@ -1,7 +1,8 @@
 package com.example.weatherapp.utils
 
 object Constants {
+    //YOUR_API_KEY
     const val BASE_URL = "https://api.openweathermap.org/"
-    const val API_KEY = "fca37e260100700636a1e3b9cb24a997"
+    const val API_KEY = "YOUR_API_KEY"
 
 }
